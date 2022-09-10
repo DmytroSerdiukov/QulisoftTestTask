f9c2c0b9611f519a295c4860ba99cc347180951f
dmitry <ds@gmail.com>
2022-09-10 15:59:47 +0300

    feature: photos Slice

--- app/store/reducers/photos.tsx
+++ app/store/reducers/photos.tsx
@@ -1,9 +1,34 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import {PhotosAPI} from '../../api';
+
+const fetchPhotos = createAsyncThunk(
+  'photos/fetchPhotos',
+  async (_, thunkAPI) => {
+    const response = await PhotosAPI.fetchPhotos();
+  },
+);
+
+interface InitState {
+  photos: any[];
+  error: any;
+}
+
+const initState: InitState = {
+  photos: [],
+  error: null,
+};
 
 export const photoSlice = createSlice({
   name: 'photos',
-  initialState: [],
+  initialState: initState,
   reducers: {},
+  extraReducers: builder => {
+    builder.addCase(fetchPhotos.pending, (state, action) => {}),
+      builder.addCase(fetchPhotos.fulfilled, (state, action) => {
+        state.photos = [...action.payload];
+      }),
+      builder.addCase(fetchPhotos.rejected, (state, action) => {});
+  },
 });
 
 export default photoSlice.reducer;
