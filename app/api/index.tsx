259d9569dd614bb8e397e3bab09f04681fc5feb2
dmitry <ds@gmail.com>
2022-09-10 22:39:25 +0300

    refactor: changed api response

--- app/api/index.tsx
+++ app/api/index.tsx
@@ -9,7 +9,7 @@ export const PhotosAPI = {
             '896d4f52c589547b2134bd75ed48742db637fa51810b49b607e37e46ab2c0043',
         },
       });
-      return response;
+      return response.data;
     } catch (e) {
       throw e;
     }
