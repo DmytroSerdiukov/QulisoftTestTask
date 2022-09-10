35f8dd14b327bc4e147eb57dd3493e61eb26bbb7
dmitry <ds@gmail.com>
2022-09-10 15:58:18 +0300

    feature: photos api

---
+++ app/api/index.tsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const PhotosAPI = {
+  fetchPhotos: async () => {
+    try {
+      const response: any = await axios.get('https://api.unsplash.com/photos', {
+        params: {
+          client_id:
+            '896d4f52c589547b2134bd75ed48742db637fa51810b49b607e37e46ab2c0043',
+        },
+      });
+      return response;
+    } catch (e) {
+      throw e;
+    }
+  },
+};
