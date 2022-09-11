023233596b11b85f7d9966f6fc4d6d64e0a01466
dmitry <ds@gmail.com>
2022-09-11 11:39:02 +0300

    feature: photo screen markup

---
+++ app/screens/Photo/index.tsx
@@ -0,0 +1,41 @@
+import {View, Image, Button, Text, StyleSheet} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {useNavigation} from '@react-navigation/native';
+
+type PhotoProps = {
+  route: any;
+};
+const Photo: React.FC<PhotoProps> = ({route}) => {
+  const {id, url} = route.params;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image
+        style={styles.img}
+        source={{uri: url}}
+        resizeMode={'cover'}
+        resizeMethod={'resize'}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Photo;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  img: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'gray',
+  },
+});
