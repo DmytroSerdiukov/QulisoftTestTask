f2c3a0f1e2f030fb1d86eda36850143990eb82a8
dmitry <ds@gmail.com>
2022-09-11 20:39:42 +0300

    refactor: App.tsx deleted unused dependencies

--- App.tsx
+++ App.tsx
@@ -1,31 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React from 'react'
+import { StyleSheet } from 'react-native'
 
-import React, { type PropsWithChildren } from 'react'
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native'
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen'
 import MainContainer from './app/screens/Main/container'
 
 import { NavigationContainer } from '@react-navigation/native'
